c0854ac3910ecc2b18331bf61b36a553e9223676
cfe059 <jokeryuma0228@gmail.com>
2022-02-16 15:42:03 +0900

    修正

--- app/Http/Controllers/CalcSq.php
+++ app/Http/Controllers/CalcSq.php
@@ -13,8 +13,7 @@ class CalcSq extends Controller
         $number = $number*$number;
 
         $Sq = 'squared';
-
-        $link = mysqli_connect('mysql57.tcagame01.sakura.ne.jp','tcagame01','tcagame2021','tcagame01_20j70009');
+        $link = mysqli_connect('localhost', 'root', '', 'network');
         $stmt = mysqli_prepare($link, "INSERT INTO results (name, result) VALUES (?, ?)");
         mysqli_stmt_bind_param($stmt, "ss", $Sq, $number);
         $result = mysqli_stmt_execute($stmt);
